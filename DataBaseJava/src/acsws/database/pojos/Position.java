1503e9483991988d779f6cf78980c4cbde9c170f
Jorge Avarias <javarias@nrao.edu>
2015-07-28 15:25:54 -0600

    Added non-tested java db implementation based on hibernate

---
+++ DataBaseJava/src/acsws/database/pojos/Position.java
@@ -0,0 +1,20 @@
+package acsws.database.pojos;
+
+public class Position {
+
+	private double az;
+	private double el;
+	
+	public double getAz() {
+		return az;
+	}
+	public void setAz(double az) {
+		this.az = az;
+	}
+	public double getEl() {
+		return el;
+	}
+	public void setEl(double el) {
+		this.el = el;
+	}
+}
